345479b24aa8d166c97cd1845896ff16f43489b3
Jessica Moncada <jessmoncada1@gmail.com>
2023-06-19 22:35:20 -0500

    Reto Final dashboard

--- resources/views/dashboard/category/index.blade.php
+++ resources/views/dashboard/category/index.blade.php
@@ -1,8 +1,8 @@
 @extends('layouts.app')
 
 @section('content')
-    <a class="btn btn-warning my-3" href="{{ route('categories.create') }}">Crear categoría</a>
-    <table class="table table-bordered">
+    <a class="btn btn-success my-3" href="{{ route('categories.create') }}">Crear categoría</a>
+    <table class="table">
         <thead>
             <tr>
                 <th>Nombre categoría</th>
@@ -16,8 +16,8 @@
                     <td>{{ $c->title }}</td>
                     <td>{{ $c->slug }}</td>
                     <td width="10px">
-                        <a class="btn btn-primary btn-info edit" href="{{ route('categories.edit', $c) }}">Editar</a>
-                        <a class="btn btn-primary btn-purple" href="{{ route('categories.show', $c) }}">Ver</a>
+                        <a class="btn btn-primary btn-action edit" href="{{ route('categories.edit', $c) }}">Editar</a>
+                        <a class="btn btn-primary btn-action" href="{{ route('categories.show', $c) }}">Ver</a>
                         <form action="{{ route('categories.destroy', $c) }}" method="POST" class="form-delete">
                             @csrf
                             @method('DELETE')
